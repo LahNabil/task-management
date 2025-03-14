26a5b8b0d0c7feb53d076fbfe1eef8838a6ad74a
Nabil <lahlalia.nabil@hotmail.com>
2025-03-14 16:01:32 +0000

    cors prod

--- src/main/java/net/lahlalia/taskmanagement/config/BeansConfig.java
+++ src/main/java/net/lahlalia/taskmanagement/config/BeansConfig.java
@@ -20,7 +20,7 @@ public class BeansConfig {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+        config.setAllowedOrigins(Collections.singletonList("http://taskawsstestbucket.s3-website.eu-west-3.amazonaws.com/"));
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.ORIGIN,
                 HttpHeaders.CONTENT_TYPE,
